8bb020c1f9a17a07c3a7a175e3105e8e11d666a3
Klaudia Kurela <klakure97@gmail.com>
2022-05-29 14:29:17 +0200

    Add design
    Add design for: dodaj pracownika, nowa faktura, przyjęcie, wydanie

---
+++ ViewResources/TowaryTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma.ViewResources
+{
+    internal class TowaryTable
+    {
+        public string Nazwa { get; set; }
+
+        public string Typ { get; set; }
+
+        public string Akt { get; set; }
+
+        public string Marza { get; set; }
+
+        public string Zaokr { get; set; }
+
+        public string Off { get; set; }
+
+        public decimal CenaN { get; set; }
+
+        public decimal CenaB { get; set; }
+
+        public string Waluta { get; set; }
+
+        public string Kod { get; set; }
+
+        public string NazwaDluga { get; set; }
+
+
+
+        public int LP { get; set; }
+
+        public string NazwaTowaru { get; set; }
+
+        public double Ilosc { get; set; }
+
+        public string Jm { get; set; }
+
+        public decimal CenaPoczatkowa { get; set; }
+
+        public decimal Rabat { get; set; }
+
+        public decimal Cena { get; set; }
+
+        public decimal Wartosc { get; set; }
+    }
+}
